b70c3c6ec38860fc074631b1fd4fba9ab07929f3
Ryan Champin <rchampin@rchampin-i.local>
2018-06-08 20:19:18 -0400

    updates with the scroll shit

--- Screens/Program/Program.js
+++ Screens/Program/Program.js
@@ -35,49 +35,51 @@ class Program extends Component {
   }
 
   toggleWeekActive() {
+    Animated.parallel([
+      Animated.timing(
+        this.state.topHeight,
+        {
+          toValue: !this.state.fullScreenSlide ? 0 : 1,
+          easing: Easing.bezier(0.55, 0, 0.1, 1),
+          duration: 300
+        }
+      ),
+      Animated.timing(
+        this.state.slideHeight,
+        {
+          toValue: this.state.fullScreenSlide ? 300 : Dimensions.get('window').height,
+          easing: Easing.bezier(0.55, 0, 0.1, 1),
+          duration: 300
+        }
+      ),
+      Animated.timing(
+        this.state.slideWidth,
+        {
+          toValue: this.state.fullScreenSlide ? Dimensions.get('window').width - 20 : Dimensions.get('window').width,
+          easing: Easing.bezier(0.55, 0, 0.1, 1),
+          duration: 300
+        }
+      ),
+      Animated.timing(
+        this.state.slideMargin,
+        {
+          toValue: this.state.fullScreenSlide ? 10 : 0,
+          easing: Easing.bezier(0.55, 0, 0.1, 1),
+          duration: 300
+        }
+      ),
+      Animated.timing(
+        this.state.slideRadius,
+        {
+          toValue: this.state.fullScreenSlide ? 10 : 0,
+          easing: Easing.bezier(0.55, 0, 0.1, 1),
+          duration: 300
+        }
+      )
+    ]).start();
     this.setState({
       fullScreenSlide: !this.state.fullScreenSlide
     });
-    Animated.timing(
-      this.state.topHeight,
-      {
-        toValue: this.state.fullScreenSlide ? 0 : 1,
-        easing: Easing.bezier(0.55, 0, 0.1, 1),
-        duration: 300
-      }
-    ).start();
-    Animated.timing(
-      this.state.slideHeight,
-      {
-        toValue: this.state.fullScreenSlide ? 300 : Dimensions.get('window').height,
-        easing: Easing.bezier(0.55, 0, 0.1, 1),
-        duration: 300
-      }
-    ).start();
-    Animated.timing(
-      this.state.slideWidth,
-      {
-        toValue: this.state.fullScreenSlide ? Dimensions.get('window').width - 20 : Dimensions.get('window').width,
-        easing: Easing.bezier(0.55, 0, 0.1, 1),
-        duration: 300
-      }
-    ).start();
-    Animated.timing(
-      this.state.slideMargin,
-      {
-        toValue: this.state.fullScreenSlide ? 10 : 0,
-        easing: Easing.bezier(0.55, 0, 0.1, 1),
-        duration: 300
-      }
-    ).start();
-    Animated.timing(
-      this.state.slideRadius,
-      {
-        toValue: this.state.fullScreenSlide ? 10 : 0,
-        easing: Easing.bezier(0.55, 0, 0.1, 1),
-        duration: 300
-      }
-    ).start();
   }
 
   renderItems() {
@@ -108,13 +110,12 @@ class Program extends Component {
         <Animated.View style={{ backgroundColor: 'orange', flex: this.state.topHeight }} />
         <Animated.ScrollView
           ref={(scrollView) => { this.scrollView = scrollView; }}
-          //pagingEnabled={true}
-          horizontal={true}
+          horizontal
           scrollEnabled={!this.state.fullScreenSlide}
           decelerationRate={0}
           snapToInterval={Dimensions.get('window').width}
-          snapToAlignment={"center"}
-          >
+          snapToAlignment={'center'}
+        >
           {weeks}
         </Animated.ScrollView>
       </View>
